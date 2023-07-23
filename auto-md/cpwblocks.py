fixed rotation precision - introduced global rounding precision - introduced global datatype float64 for numpy arrays - fixed low rounding precision which caused errors when rotating structures

auto-md/cpwblocks.py
@@ -1,7 +1,7 @@
 from numpy.typing import NDArray
 import numpy as np
-from shapes import PLine2d, PolyArc
-from utils import rotate_vec, tuple_to_rad, to_rad
+from shapes import PolyArc, PolyRectangle, PLine2d
+from utils import rotate_vec, tuple_to_rad, to_rad, dtype
 from acad_api import Acad
 
 from typing import Union
@@ -21,6 +21,7 @@ class CPWSection:
         self.anchor = anchor
         self.elements = None
         self.endpoints = None
+        self.length = None
 
     def __repr__(self):
         return f"{self.name}: anchor={self.anchor}"
@@ -31,12 +32,13 @@ class CPWStraight(CPWSection):
         super(CPWStraight, self).__init__(**kwargs)
 
         self.angle = angle
+        self.rad_angle = to_rad(self.angle)
         self.length = length
 
         l_points = np.array([[0, 0],
                              [self.length, 0],
                              [self.length, self.width],
-                             [0, self.width]])
+                             [0, self.width]], dtype=dtype)
 
         u_points = l_points.copy()
         u_points[:, 1] += self.gap + self.width
@@ -67,12 +69,14 @@ class CPWArc(CPWSection):
                      'outer inner': self.radius + 0.5 * self.gap,
                      'outer outer': self.radius + 0.5 * self.gap + self.width}
 
+        self.length = self.angle / (2 * np.pi) * self.radius
+
         sincos = np.array([np.cos(self.angle), np.sin(self.angle)])
 
         inner_points = np.array([[self.rads['inner inner'], 0], [self.rads['inner outer'], 0],
-                                 self.rads['inner outer'] * sincos, self.rads['inner inner'] * sincos])
+                                 self.rads['inner outer'] * sincos, self.rads['inner inner'] * sincos], dtype=dtype)
         outer_points = np.array([[self.rads['outer inner'], 0], [self.rads['outer outer'], 0],
-                                 self.rads['outer outer'] * sincos, self.rads['outer inner'] * sincos])
+                                 self.rads['outer outer'] * sincos, self.rads['outer inner'] * sincos], dtype=dtype)
 
         for i, vec in enumerate(inner_points):
             inner_points[i] = rotate_vec(vec, self.angle_span[0], rad=True)
@@ -84,4 +88,10 @@ class CPWArc(CPWSection):
         inner_points += shift
         outer_points += shift
 
+        r_ends = CPWEndpoint(
+            ends=np.array([inner_points[0], inner_points[1], outer_points[0], outer_points[1]], dtype=dtype))
+        l_ends = CPWEndpoint(
+            ends=np.array([inner_points[3], inner_points[2], outer_points[3], inner_points[2]], dtype=dtype))
+
         self.elements = [PolyArc(inner_points, angle=self.angle), PolyArc(outer_points, angle=self.angle)]
+        self.endpoints = (l_ends, r_ends)

auto-md/shapes.py
@@ -2,12 +2,13 @@ import numpy as np
 from numpy.typing import NDArray
 from abc import ABC, abstractmethod
 from acad_api import Acad
-from utils import to_rad
+from utils import to_rad, rounding_precision, dtype
 
 
 class Shape(ABC):
-    def __init__(self):
+    def __init__(self, roun_prec=rounding_precision):
         self.acad_object = None
+        self.round_prec = roun_prec
 
     @abstractmethod
     def draw(self, acad: Acad):
@@ -20,7 +21,7 @@ class Shape(ABC):
 class Point2d(Shape):
     def __init__(self, x: float, y: float):
         super().__init__()
-        self.point = np.array([x, y, 0], dtype='float64')
+        self.point = np.round(np.array([x, y, 0], dtype=dtype), self.round_prec)
 
     def draw(self, acad: Acad):
         self.acad_object = acad.model.AddPoint(self.point)
@@ -31,14 +32,14 @@ class PLine2d(Shape):
         super().__init__()
         assert points.shape[1] == 2
 
-        self.points = np.zeros((points.shape[0] + 1, 2))
+        self.points = np.zeros((points.shape[0] + 1, 2), dtype=dtype)
 
         for i, p in enumerate(points):
-            self.points[i] = np.array([p[0], p[1]], dtype='float64')
+            self.points[i] = np.array([p[0], p[1]], dtype=dtype)
 
         self.points[-1] = self.points[0]
 
-        self.points = self.points.flatten()
+        self.points = np.round(self.points.flatten(), self.round_prec)
 
     def draw(self, acad: Acad):
         self.acad_object = acad.model.AddLightweightPolyline(self.points)
@@ -49,10 +50,21 @@ class PolyArc(PLine2d):
     def __init__(self, points: NDArray, angle: float):
         super(PolyArc, self).__init__(points=points)
         self.angle = angle
+        self.bulge = np.round(np.tan(0.25 * self.angle), self.round_prec)
 
     def draw(self, acad: Acad):
         self.acad_object = acad.model.AddLightweightPolyline(self.points)
         self.acad_object.closed = True
-        self.acad_object.SetBulge(1, np.tan(0.25 * self.angle))
-        self.acad_object.SetBulge(3, -np.tan(0.25 * self.angle))
+        self.acad_object.SetBulge(1, self.bulge)
+        self.acad_object.SetBulge(3, -self.bulge)
 
+
+class PolyRectangle(PLine2d):
+    def __init__(self, points: NDArray, angle: float):
+        super(PolyRectangle, self).__init__(points=points)
+        self.angle = angle
+        self.base_point = np.array([np.average(self.points[0::2]), np.average(self.points[1::2]), 0], dtype=dtype)
+
+    def draw(self, acad: Acad):
+        super(PolyRectangle, self).draw(acad=acad)
+        self.acad_object.Rotate(self.base_point, self.angle)

auto-md/utils.py
@@ -1,14 +1,17 @@
 import numpy as np
 from numpy.typing import NDArray
 
+rounding_precision = 10
+dtype = 'float64'
+
 
 def rotate_vec(vec: NDArray, theta: float, rad: bool = False):
     assert vec.shape == (2,)
     if not rad:
-        theta = (theta / 180.) * np.pi
+        theta = (theta / 180.0) * np.pi
 
-    rot_mat = np.array([[np.cos(theta), -np.sin(theta)],
-                        [np.sin(theta), np.cos(theta)]])
+    rot_mat = np.round(np.array([[np.cos(theta), -np.sin(theta)],
+                                 [np.sin(theta), np.cos(theta)]], dtype='float64'), rounding_precision)
 
     return np.dot(rot_mat, vec)
 